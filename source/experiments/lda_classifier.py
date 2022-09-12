db64e65175ce20103ec2d0ba80d65e15cb34a00e
Christian Hall <ch0164@uah.edu>
2022-09-12 09:27:59 -0500

    Adding null flares without training does not perform well

--- source/experiments/lda_classifier.py
+++ source/experiments/lda_classifier.py
@@ -23,100 +23,102 @@ def main() -> None:
     # ------------------------------------------------------------------------
     # Pre-process the data for the experiment.
     # Afterwards, place this data in `cleaned_data_directory`.
-    needed_where = 8
-    average_over = 12
-
-    needed_flare_classes = FLARE_CLASSES[1:]
-    print(needed_flare_classes)
-    # flare_dataframes = [
-    #     get_ar_properties(flare_class, needed_where, average_over)
-    #     for flare_class in needed_flare_classes
-    # ]
+    center_time = 16
+    time_interval = 12
 
     # Get the time window of the experiment for metadata.
-    lo_time = int(24 - (needed_where + average_over // 2))
-    hi_time = int(24 - (needed_where - average_over // 2))
+    lo_time = int((center_time - time_interval // 2))
+    hi_time = int((center_time + time_interval // 2))
     time_window = f"{lo_time}h_{hi_time}h"
     time_window_caption = time_window.replace("_", "-")
 
-    # # Save our original data.
-    # for flare_class, df in zip(needed_flare_classes, flare_dataframes):
-    #     df.to_csv(f"{cleaned_data_directory}{flare_class.lower()}_"
-    #               f"{time_window}_mean_dataset_{now_string}.csv")
+    # Obtain the properties for flares.
+    flare_dataframes = [
+        get_ar_properties(flare_class,
+                          lo_time,
+                          hi_time,
+                          cleaned_data_directory,
+                          now_string,
+                          wipe_old_data=True
+        )
+        for flare_class in FLARE_CLASSES
+    ]
 
-    # Perform LDA with two classes: B and MX.
-    b_df, mx_df = \
-        pd.read_csv(f"{cleaned_data_directory}b_10h_22h_mean_dataset_07_09_2022_13_42_35.csv"), \
-        pd.read_csv(f"{cleaned_data_directory}mx_10h_22h_mean_dataset_07_09_2022_13_42_35.csv")
-    b_df = b_df.loc[b_df["xray_class"] == "B"]
-    mx_df["xray_class"] = "MX"
-    bmx_df = pd.concat([b_df, mx_df]).dropna()
+    # Train LDA classifier with two classes: B and MX.
+    # Then, test LDA classifier with NULL flares.
+    null_df, train_b_df, train_mx_df = tuple(flare_dataframes)
+    train_b_df = train_b_df.loc[train_b_df["xray_class"] == "B"]
+    train_mx_df["xray_class"] = "MX"
+    null_df["xray_class"] = "N"
+    print(null_df.dropna())
 
-    # target = bmx_df["xray_class"]
-    #
-    # train, test = train_test_split(bmx_df, test_size=0.0)
+    train_b_df, test_b_df = train_test_split(train_b_df, test_size=0.3)
+    train = pd.concat([train_b_df, train_mx_df]).dropna()
+    train_target = train["xray_class"]
+    test = pd.concat([null_df, test_b_df]).dropna()
+    test_target = test["xray_class"]
 
-    lda = LinearDiscriminantAnalysis()
-    components = lda.fit_transform(bmx_df[FLARE_PROPERTIES], bmx_df["xray_class"])
+    train_lda = LinearDiscriminantAnalysis()
+    train_components = train_lda.fit_transform(train[FLARE_PROPERTIES], train_target)
 
     ld_labels = [f"LD{i + 1}" for i in range(1)]
-    lda_df = pd.DataFrame(components, columns=ld_labels)
-    lda_df["xray_class"] = list(bmx_df["xray_class"])
+    train_lda_df = pd.DataFrame(train_components, columns=ld_labels)
+    train_lda_df["xray_class"] = list(train_target)
 
-    b_df = lda_df.loc[lda_df["xray_class"] == "B"]
-    b_count = b_df.shape[0]
-    mx_df = lda_df.loc[lda_df["xray_class"] == "MX"]
-    mx_count = mx_df.shape[0]
+    train_b_df = train_lda_df.loc[train_lda_df["xray_class"] == "B"]
+    train_b_count = train_b_df.shape[0]
+    train_mx_df = train_lda_df.loc[train_lda_df["xray_class"] == "MX"]
+    train_mx_count = train_mx_df.shape[0]
 
-    b_centroid = b_df["LD1"].mean()
-    mx_centroid = mx_df["LD1"].mean()
+    b_centroid = train_b_df["LD1"].mean()
+    mx_centroid = train_mx_df["LD1"].mean()
     midpoint = (b_centroid + mx_centroid) / 2
 
-    # test_lda = LinearDiscriminantAnalysis()
-    # test_components = test_lda.fit_transform(test[FLARE_PROPERTIES], test["xray_class"])
-    #
-    # ld_labels = [f"LD{i + 1}" for i in range(1)]
-    # test_lda_df = pd.DataFrame(test_components, columns=ld_labels)
-    # test_lda_df["xray_class"] = list(test["xray_class"])
+    test_lda = LinearDiscriminantAnalysis()
+    print(test["xray_class"])
+    test_components = test_lda.fit_transform(test[FLARE_PROPERTIES],
+                                             test_target)
 
-    # b_df = test_lda_df.loc[test_lda_df["xray_class"] == "B"]
-    # mx_df = test_lda_df.loc[test_lda_df["xray_class"] == "MX"]
-
-    b_df = lda_df.loc[lda_df["xray_class"] == "B"]
-    mx_df = lda_df.loc[lda_df["xray_class"] == "MX"]
+    ld_labels = [f"LD{i + 1}" for i in range(1)]
+    test_lda_df = pd.DataFrame(test_components, columns=ld_labels)
+    test_target = test_lda_df["xray_class"] = "NB"
+    test_lda_centroid = test_lda_df["LD1"].mean()
 
     # sns.set_palette(sns.color_palette(["#FF0B04", "#4374B3"]))
     fig, ax = plt.subplots()
-    b_df["jitter"] = [random.uniform(0, 1) for _ in range(b_df.shape[0])]
-    mx_df["jitter"] = [random.uniform(0, 1) for _ in range(mx_df.shape[0])]
+    train_b_df["jitter"] = [random.uniform(0, 1) for _ in range(train_b_df.shape[0])]
+    train_mx_df["jitter"] = [random.uniform(0, 1) for _ in range(train_mx_df.shape[0])]
+    test_lda_df["jitter"] = [random.uniform(0, 1) for _ in range(test_lda_df.shape[0])]
+    print(test_lda_df)
 
-    b_df.to_csv(f"{cleaned_data_directory}b_{time_window}_mean_lda_{now_string}.csv")
-    mx_df.to_csv(f"{cleaned_data_directory}mx_{time_window}_mean_lda_{now_string}.csv")
-    b_df.plot(x="LD1", y="jitter", label="B", kind="scatter", c="dodgerblue", ax=ax)
-    mx_df.plot(x="LD1", y="jitter", label="MX", kind="scatter", c="orangered", ax=ax)
+    train_b_df.plot(x="LD1", y="jitter", label="Train B", kind="scatter", c="dodgerblue", ax=ax)
+    train_mx_df.plot(x="LD1", y="jitter", label="MX", kind="scatter", c="orangered", ax=ax)
+    test_lda_df.plot(x="LD1", y="jitter", label="NB", kind="scatter", c="grey", ax=ax)
 
     ax.scatter([b_centroid], [0.5], color="k", marker='X')
     ax.scatter([mx_centroid], [0.5], color="k", marker='X')
+    ax.scatter([test_lda_centroid], [0.5], color="k", marker='X')
     ax.scatter([midpoint], [0.5], color="k", marker='X')
     ax.axvline(x=midpoint, color="k")
 
-    plt.title(f"{experiment_caption} on B/MX {time_window_caption} Mean")
+    plt.title(f"{experiment_caption} on Null Flares {time_window_caption} Mean,"
+              f"Trained by B/MX Flares")
     plt.tight_layout()
     plt.savefig(
-        f"{figure_directory}b_mx_scatterplot_{time_window}_mean_{now_string}.png")
+        f"{figure_directory}null_b_mx_scatterplot_{time_window}_mean_{now_string}.png")
     plt.show()
 
     y_pred = []
-    df = pd.concat([b_df, mx_df])
-    for index, row in df.iterrows():
+    for index, row in test_lda_df.iterrows():
         x = row["LD1"]
         if x <= midpoint:
-            y_pred.append("B")
+            # y_pred.append("B")
+            y_pred.append("NB")
         else:
             y_pred.append("MX")
 
     from sklearn.metrics import confusion_matrix
-    tp, fn, fp, tn = confusion_matrix(bmx_df["xray_class"], y_pred).ravel()
+    tp, fn, fp, tn = confusion_matrix(test_target, y_pred).ravel()
 
     # calculate accuracy
     conf_accuracy = (float(tp + tn) / float(tp + tn + fp + fn))
@@ -137,10 +139,9 @@ def main() -> None:
     with open(f"{other_directory}b_mx_{time_window}_mean_classification_metrics_{now_string}.txt", "w", newline="\n") as f:
         f.write("Classification Metrics\n")
         f.write('-' * 50 + "\n")
-        f.write(f"Total # of B Flares: {b_count}\n")
-        f.write(f"Total # of MX Flares: {mx_count}\n")
-        f.write("Training Size: 0.7\n")
-        f.write("Testing Size: 0.3\n")
+        f.write(f"Total # of B Flares: {train_b_count}\n")
+        f.write(f"Total # of MX Flares: {train_mx_count}\n")
+        f.write(f"Total # of NULL Flares: {test_null_count}\n")
         f.write('-' * 50 + "\n")
         f.write(f'Accuracy: {round(conf_accuracy, 2)}\n')
         f.write(f'B Recall/Sensitivity: {round(conf_sensitivity, 2)}\n')
