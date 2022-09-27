1f6b637f68d080626fcd2d74da18373e7c170b70
ch0164 <ch0164@uah.edu>
2022-09-27 15:57:37 -0500

    Classifier names were incorrect...

--- source/experiments/tabulate_classification_metrics.py
+++ source/experiments/tabulate_classification_metrics.py
@@ -3,51 +3,36 @@ import pandas as pd
 from source.utilities import *
 
 def main():
-    holdout_dict = {"all": [], "coincident": [], "noncoincident": []}
-    loo_dict = {"all": [], "coincident": [], "noncoincident": []}
-
-    names = [
-        "LDA",
-        "QDA",
-        "KNN 2",
-        "KNN 3",
-        "KNN 4",
-        "NB",
-        "Logistic Regression",
-        "Linear SVM",
-        "RBF SVM",
-        "Decision Tree",
-        "Random Forest",
-        "AdaBoost",
-    ]
-
-    time_window = "0h_24h"
-
-    for cv, d in zip(["70_30_train_test", "leave_one_out"], [holdout_dict, loo_dict]):
-        dir = RESULTS_DIRECTORY + "classification/metrics/" + cv + "/"
-        clf_names = []
-        for coincidence in COINCIDENCES:
-            for file in os.listdir(dir + coincidence):
-                if "b_mx_lda" not in file:
-                    continue
-                if time_window not in file:
-                    continue
-                if "neural_net" in file:
-                    continue
-                clf_name, tss = None, None
-                with open(dir + coincidence + "/" + file, "r") as f:
-                    for line in f:
-                        if line.endswith("Classification Metrics\n"):
-                            clf_name = line.split(" Classification Metrics")[0]
-                            clf_names.append(clf_name)
-                        delim = "True Skill Score: "
-                        if line.startswith(delim):
-                            tss = float(line.strip(delim))
-                            d[coincidence].append(tss)
-                            break
-        df = pd.DataFrame(d, columns=COINCIDENCES).rename_axis("classifier")
-        df.index = names
-        df.to_csv(dir + f"{time_window}_b_mx_lda_true_skill_score_summary.csv")
+    for flare_classification in ["b_mx", "bc_mx", "b_mx_lda", "bc_mx_lda"]:
+        for time_window in ["0h_24h", "10h_22h"]:
+            holdout_dict = {"all": [], "coincident": [], "noncoincident": []}
+            loo_dict = {"all": [], "coincident": [], "noncoincident": []}
+            for cv, d in zip(["70_30_train_test", "leave_one_out"], [holdout_dict, loo_dict]):
+                dir = RESULTS_DIRECTORY + "classification/metrics/" + cv + "/"
+                clf_names = set()
+                for coincidence in COINCIDENCES:
+                    print(os.listdir(dir + coincidence))
+                    for file in os.listdir(dir + coincidence):
+                        if f"{flare_classification}_{time_window}" not in file:
+                            continue
+                        if "neural_net" in file:
+                            continue
+                        with open(dir + coincidence + "/" + file, "r") as f:
+                            for line in f:
+                                if line.endswith("Classification Metrics\n"):
+                                    clf_name = line.split(" Classification Metrics")[0]
+                                    clf_names.add(clf_name)
+                                delim = "True Skill Score: "
+                                if line.startswith(delim):
+                                    tss = float(line.strip(delim))
+                                    d[coincidence].append(tss)
+                                    break
+                df = pd.DataFrame(d, columns=COINCIDENCES).rename_axis("classifier")
+                print(clf_names)
+                print(df)
+                df.index = clf_names
+                df.rename_axis("classifier", inplace=True)
+                df.to_csv(dir + f"{time_window}_{flare_classification}_true_skill_score_summary.csv")
 
 
 if __name__ == "__main__":
